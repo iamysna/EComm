1e09b4de64460b7b57496f667ba3a703fa9a2ddb
rukmini <rpatnala1990@gmail.com>
2016-02-08 11:36:46 +0530

    Login and Registration page Modification

--- EComm/src/main/java/com/ecomm/trail/Controller/HomeController.java
+++ EComm/src/main/java/com/ecomm/trail/Controller/HomeController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ecomm.trail.forms.Login;
+import com.ecomm.trail.forms.Registration;
 
 /**
  * Handles requests for the application home page.
@@ -29,16 +30,27 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView home(Locale locale, Model model) {
+	public String home() {
+	  return "home";
+	   }
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(Locale locale, Model model) {
 		
-		return new ModelAndView("home","login",new Login());
+		return new ModelAndView("login","login",new Login());
 	}
 	
-	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public String login(@ModelAttribute("login") Login login, BindingResult result, ServletResponse response,ModelAndView mv)
+	@RequestMapping(value = "/Registration", method = RequestMethod.GET)
+	public ModelAndView Registration(Locale locale, Model model) {
+		
+		return new ModelAndView("Registration","registration",new Registration());
+	}
+		
+	/*@RequestMapping(value="/proceed", method=RequestMethod.POST)
+	public String proceed(@ModelAttribute("login") Login login, BindingResult result, ServletResponse response,ModelAndView mv)
 	{
 		mv.addObject("uname", login.getUname());
 		return "login";
-	}
+	}*/
 	
 }
