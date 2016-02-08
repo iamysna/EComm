f0e09db8d67ca2d1ec16dbe70cb96107d247b39e
Srilatha <srilathapamarthi@gmail.com>
2016-02-08 10:34:44 +0530

    Pojos created

---
+++ EComm/src/main/java/com/ecomm/pojo/Customer.java
@@ -0,0 +1,32 @@
+package com.ecomm.pojo;
+
+public class Customer {
+	private Integer customerId;
+	private Integer productId;
+
+	/**
+	 * @return the customerId
+	 */
+	public Integer getCustomerId() {
+		return customerId;
+	}
+	/**
+	 * @param customerId the customerId to set
+	 */
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+	/**
+	 * @return the productId
+	 */
+	public Integer getProductId() {
+		return productId;
+	}
+	/**
+	 * @param productId the productId to set
+	 */
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	
+}
---
+++ EComm/src/main/java/com/ecomm/pojo/Product.java
@@ -0,0 +1,57 @@
+package com.ecomm.pojo;
+
+public class Product {
+private Integer productId;
+private String productName;
+private String description;
+private Integer price;
+/**
+ * @return the productId
+ */
+public Integer getProductId() {
+	return productId;
+}
+/**
+ * @param productId the productId to set
+ */
+public void setProductId(Integer productId) {
+	this.productId = productId;
+}
+/**
+ * @return the productName
+ */
+public String getProductName() {
+	return productName;
+}
+/**
+ * @param productName the productName to set
+ */
+public void setProductName(String productName) {
+	this.productName = productName;
+}
+/**
+ * @return the description
+ */
+public String getDescription() {
+	return description;
+}
+/**
+ * @param description the description to set
+ */
+public void setDescription(String description) {
+	this.description = description;
+}
+/**
+ * @return the price
+ */
+public Integer getPrice() {
+	return price;
+}
+/**
+ * @param price the price to set
+ */
+public void setPrice(Integer price) {
+	this.price = price;
+}
+
+}
